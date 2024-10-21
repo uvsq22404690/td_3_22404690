fa52f5ef0cfed658cee1b478a65975090fc1eafc
uvsq22404690 <riadnamane@gmail.com>
2024-10-21 11:06:26 +0200

    Ajout de commentaires dans la classe DnsItemTest pour expliquer les tests de création et de représentation en chaîne des objets DnsItem

--- src/test/java/dns/DnsItemTest.java
+++ src/test/java/dns/DnsItemTest.java
@@ -4,22 +4,26 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DnsItemTest {
+    // Test de la création d'un DnsItem
     @Test
     public void testDnsItemCreation() {
         NomMachine nomMachine = new NomMachine("www.example.com");
         AdresseIP adresseIP = new AdresseIP("192.168.1.1");
         DnsItem dnsItem = new DnsItem(nomMachine, adresseIP);
 
+        // Vérifie que les attributs sont correctement assignés
         assertEquals(nomMachine, dnsItem.getNomMachine());
         assertEquals(adresseIP, dnsItem.getAdresseIP());
     }
 
+    // Test de la méthode toString de DnsItem
     @Test
     public void testToString() {
         NomMachine nomMachine = new NomMachine("www.example.com");
         AdresseIP adresseIP = new AdresseIP("192.168.1.1");
         DnsItem dnsItem = new DnsItem(nomMachine, adresseIP);
 
+        // Vérifie que la représentation sous forme de chaîne est correcte
         assertEquals("www.example.com 192.168.1.1", dnsItem.toString());
     }
 }
