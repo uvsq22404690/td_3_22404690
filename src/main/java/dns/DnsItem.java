4ce38bc2d30dc456d8f40cac38c664ec5e14e6f1
uvsq22404690 <riadnamane@gmail.com>
2024-10-21 10:57:39 +0200

    Ajout de la classe DnsItem avec les attributs nomMachine et adresseIP. Implémentation des méthodes d'accès et de la méthode toString pour une représentation textuelle.

--- src/main/java/dns/DnsItem.java
+++ src/main/java/dns/DnsItem.java
@@ -1,24 +1,28 @@
 package dns;
 
 public class DnsItem {
-    private NomMachine nomMachine;
-    private AdresseIP adresseIP;
+    private NomMachine nomMachine; // Nom de la machine
+    private AdresseIP adresseIP; // Adresse IP associée à la machine
 
+    // Constructeur pour initialiser un DnsItem avec un nom de machine et une adresse IP
     public DnsItem(NomMachine nomMachine, AdresseIP adresseIP) {
-        this.nomMachine = nomMachine;
-        this.adresseIP = adresseIP;
+        this.nomMachine = nomMachine; // Assignation du nom de machine
+        this.adresseIP = adresseIP; // Assignation de l'adresse IP
     }
 
+    // Méthode pour obtenir le nom de la machine
     public NomMachine getNomMachine() {
-        return nomMachine;
+        return nomMachine; // Retourne l'objet NomMachine
     }
 
+    // Méthode pour obtenir l'adresse IP
     public AdresseIP getAdresseIP() {
-        return adresseIP;
+        return adresseIP; // Retourne l'objet AdresseIP
     }
 
+    // Méthode toString pour afficher le DnsItem sous forme de chaîne
     @Override
     public String toString() {
-        return nomMachine + " " + adresseIP;
+        return nomMachine + " " + adresseIP; // Retourne une représentation textuelle de l'item
     }
 }
